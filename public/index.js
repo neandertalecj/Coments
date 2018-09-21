d7be7617414735da444a2dd74ac8d4bdce504813
Vasyl Kurtash <vasyl.kurtash@gmail.com>
2018-09-21 17:23:30 +0300

    Tried to recive post method

--- public/index.js
+++ public/index.js
@@ -1,1 +1,38 @@
 // alert('Hello from coments');
+// var com = document.getElementsByTagName('form')[0].elements[0].value;
+
+function sub() {
+	var com = document.getElementsByTagName('form')[0].elements[0].value;
+	var messErr = document.getElementById('messErr');
+	if (com === '') {
+		messErr.innerHTML = 'Enter some text? please!';
+	} else {
+			messErr.innerHTML = '';
+
+	}
+	console.log(com);
+	sendToServer(com);
+}
+
+function sendToServer(com) {
+	const data ={
+		text: com
+	}
+
+	console.log(data);
+
+	const header = new Headers();
+
+	const init = {
+		method: 'POST',
+		headers: header,
+		body: JSON.stringify(data)
+	}
+
+	const request = new Request('/content', init);
+
+	fetch(request)
+		.then((res) => res )
+		.then((res) => console.log(res.status) )
+		.catch((err) => console.log(err))
+}
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const fs = require('fs');
+const bodyParser = require('body-parser');
+
+// const jsonParser = bodyParser.json();
 
 const app = express();
 
@@ -7,8 +10,14 @@ app.use(express.static('public'));
 
 app.get('/content', (req, res) => res.sendFile(__dirname + './public/content.txt'));
 
+// parse application/json
+app.use(bodyParser.json());
+
 app.post('/content', (req, res) => {
-	fs.writeFile('./public/content.txt', req.body, console.log(req.body));
+	// fs.writeFile('./public/content.txt', req.body, console.log(req.body));
+	console.log(req.body);
+	// console.log(JSON.parse(req.body));
+	res.sendStatus(200);
 });
 
 const port = process.env.PORT || 3000;
